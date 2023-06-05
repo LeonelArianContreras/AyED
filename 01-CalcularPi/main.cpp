19860b3d9289c2b13397a78707c9896be265081d
LeonelArianContreras <135293133+LeonelArianContreras@users.noreply.github.com>
2023-06-04 22:48:55 -0300

    Update main.cpp

--- 01-CalcularPi/main.cpp
+++ 01-CalcularPi/main.cpp
@@ -3,21 +3,21 @@
 #include<iomanip>
 using namespace std;
 
-	int main() {								//Creo la funcion int main, donde declaro las constantes y la variable.
+	int main() {
 	double phi=4,x=-3,suc;                    
-		for(int i=0;i<=1000000;i++){
-			suc=(4/x);							
-			phi+=suc;							//Ingreso un while para que se itere esa cantidad de veces la variable "suc", para luego sumarle "phi".
-				if(suc<0){						
+		while(phi<=(3.141592) || phi>=(3.1415924)){
+			suc=(4/x);
+			phi+=suc;
+				if(suc<0){
 					x=(x-2)*(-1);
-				}								//Ingreso dos condiciones para que se pueda alternar la suma y resta como se expresa en la fórmula de Leibniz. 
-				else if(suc>0){					//Si suc es negativo, entonces restale dos y convertilo en positivo. Si es positivo, sumale dos y convertilo en negativo.
-					x=(x+2)*(-1);				//De esta forma logre la intercalacion de suma y resta, y que el denominador vaya de numero primo en primo.
+				}
+				else if(suc>0){
+					x=(x+2)*(-1);
 				}
 										
 		}
 		
-		cout<<setprecision(7)<<phi;				//Para poder acotar los decimales en 6 cifras, use la libreria dada junto con este codigo.
+		cout<<setprecision(7)<<phi;
 	
 		return 0;
 	}	
