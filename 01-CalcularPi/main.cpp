8ffe88f300620c74a5ceddcec615b77c81bb46cc
LeonelArianContreras <lecontreras@frba.utn.edu.ar>
2023-06-22 22:51:06 -0300

    Update main.cpp

--- 01-CalcularPi/main.cpp
+++ 01-CalcularPi/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 	int main() {
 	double phi=4,x=-3,suc;                    
-		while(phi<=(3.141592) || phi>=(3.1415924)){
+		while(phi<=(3.141592) || phi>=(3.1415924)){                //Ingreso un while para que se itere entre esa cantidad de veces la variable "suc", para luego sumarle "phi".
 			suc=(4/x);
 			phi+=suc;
 				if(suc<0){
